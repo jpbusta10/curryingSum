bf49618d82f23415c1e7d5cb15123a103656f5b2
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-02-01 12:10:53 -0300

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+function sum(num1){
+        return function( num2){
+            if(num2){
+                return sum(num1+num2);
+            }
+            return num1;
+        }
+}
+console.log(sum(2));
